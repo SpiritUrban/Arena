5f3ad317567bf491b1b6f3547f5d038efe78e167
Vitaliy Dyachuk <shadespiritenator@gmail.com>
2023-11-11 23:16:50 +0200

    damage

--- Player.gd
+++ Player.gd
@@ -5,6 +5,9 @@ extends CharacterBody3D
 @onready var camera = $Camera3D
 @onready var anim_player = $AnimationPlayer
 @onready var muzzle_flash = $Camera3D/Pistol/MuzzleFlash
+@onready var raycast = $Camera3D/RayCast3D
+
+var health = 3
 
 # Определение констант для скорости движения и силы прыжка.
 const SPEED = 10.0
@@ -14,13 +17,19 @@ const JUMP_VELOCITY = 10.0
 # var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var gravity = 20.0
 
+func _enter_tree():
+	set_multiplayer_authority(str(name).to_int())
+
 # Функция, вызываемая при инициализации узла.
 func _ready():
+	if not is_multiplayer_authority(): return
 	# Устанавливаем режим мыши на "захваченный", чтобы курсор был скрыт.
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	camera.current = true
 
 # Функция обработки входящих событий.
 func _unhandled_input(event):
+	if not is_multiplayer_authority(): return
 	# Обрабатываем движение мыши для управления вращением.
 	if event is InputEventMouseMotion:
 		rotate_y(-event.relative.x * .005)
@@ -29,9 +38,14 @@ func _unhandled_input(event):
 	
 	if Input.is_action_just_pressed("shoot") \
 			and anim_player.current_animation != "shoot":
-		play_shoot_effects()
+		play_shoot_effects.rpc()
+		if raycast.is_colliding():
+			var hit_player = raycast.get_collider()
+			hit_player.receive_damage.rpc_id(hit_player.get_multiplayer_authority())
+			
 # Функция, вызываемая на каждом физическом шаге обновления.
 func _physics_process(delta):
+	if not is_multiplayer_authority(): return
 	# Применяем гравитацию, если персонаж не на полу.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
@@ -60,8 +74,21 @@ func _physics_process(delta):
 	# Обновляем положение персонажа на основе его скорости.
 	move_and_slide()
 
+@rpc("call_local")
 func play_shoot_effects():
 	anim_player.stop()
 	anim_player.play("shoot")
 	muzzle_flash.restart()
 	muzzle_flash.emitting = true
+
+@rpc("any_peer")
+func receive_damage():
+	health -= 1
+	if health <= 0:
+		health = 3
+		position = Vector3.ZERO
+	#health_changed.emit(health)
+
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == "shoot":
+		anim_player.play("idle")
